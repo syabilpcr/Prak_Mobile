c750af1485a04f8e97926e5a9303c13ba4f59abe
lindyhanan <lindy24si@mahasiswa.pcr.ac.id>
2026-04-01 10:29:10 +0700

    pertemuan_3

---
+++ Bastard_2SIC/app/src/main/java/com/example/bastardapps/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,46 @@
+package com.example.bastardapps.pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bastardapps.R
+import com.example.bastardapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //val inputNoTujuan : EditText = findViewById(R.id.inputNoTujuan)
+        //val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val nama = binding.inputNoTujuan.text
+            Log.e("Klik btnSubmit", "Tombol berhasil di tekan. Isi dari inputNama = $nama")
+
+            Toast.makeText(this, "Anda telah melakukan klik pada tombol Submit", Toast.LENGTH_SHORT)
+                .show()
+
+            val intent = Intent(this, ThirdResultAcitivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
